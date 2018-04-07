a5542316d4a39ae84bbfb4225ce447cfb1dbab13
Bicsz note <bicsz.do@gmail.com>
2018-04-07 14:25:23 +0300

    Созданно окно, на нем интерфейс. Подключение к БД отсутствует

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,31 @@
 package sample;
 
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
 public class Controller {
+    @FXML
+    private Button button;
+
+    @FXML
+    public void onClickMethod(){
+        button.setText("Thanks!");
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("lab1.fxml"));
+            Parent root1 = (Parent) fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.setTitle("GPS треки отправка на сервер");
+            stage.setScene(new Scene(root1));
+
+            stage.show();
+
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,14 +11,16 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Menu");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
+
     }
 
 
     public static void main(String[] args) {
         launch(args);
     }
-}
 
+
+}
